982c672eac595aa12f5b73b1171cc21d9c550732
ulloa <samuel.ulloa@gmail.com>
2017-12-29 15:57:41 +0000

    shell script in progress for getting data and 500blocks at a time

--- getData.R
+++ getData.R
@@ -7,40 +7,57 @@ datasetNum <- set
 blockHeight <- as.numeric(args[[2]])
 qtyCores <- as.numeric(args[[3]])
 
-print(paste("set:", set))
-print(paste("blockheight:", blockHeight))
-print(paste("qtyCores:", qtyCores))
+inc <- 500
 
-
-inc <- 150
+totalInc <- inc * qtyCores
 i <- 0 + inc * set
-j <- 149 + inc * set
+j <- 499 + inc * set
 
 ## maxSets adds one to account for our index of 0
+maxHeightAt500 <- (400000 %/% (qtyCores * 500)) * qtyCores * 500
 maxHeightAt150 <- (498000 %/% (qtyCores * 150)) * qtyCores * 150
 
 while (i < blockHeight) {
-    if (j > blockHeight) {
-        j <- blockHeight
-    }
     
     myUrl <- paste0("https://api.blockchair.com/bitcoin/outputs?q=is_spent(0),block_id(", i, "..", j, ")&fields=recipient,value&export=csv")
 
     x <- getURL(myUrl)
     out <- read.csv(textConnection(x))
-    write.csv(out, paste0('./data/data', datasetNum, '.csv'), row.names=FALSE)
-    ## wrong
-    ## set <- set + qtyCores ## the number of cores. should be a variable
 
-    ## replaced set with qtyCores
-    i <- i + inc * qtyCores
-    j <- j + inc * qtyCores
+    if (ncol(out) == 2) {
+    
+        write.csv(out, paste0('./data/data', datasetNum, '.csv'), row.names=FALSE)
+
+    } else {
+        print(datasetNum)
+        while (ncol(out) != 2) {
+            Sys.sleep(20)
+            x <- getURL(myUrl)
+            out <- read.csv(textConnection(x))
+            if (ncol(out) == 2) {
+                write.csv(out, paste0('./data/data', datasetNum, '.csv'), row.names=FALSE)
+            }
+        }      
+    }
+
+    i <- i + totalInc
+    j <- j + totalInc
     datasetNum <- datasetNum + qtyCores
     
+    if (i >= maxHeightAt500 && inc == 500 ) {
+        ## this condition should only be true once
+        ## 498000 will be struck on set 0, otherwise we would need to make it more complex
+        inc <- 150
+        totalInc <- inc * qtyCores
+        i <- i - 350 * set
+        j <- j - 350 * (set + 1)
+    }
+
     if (i >= maxHeightAt150 && inc == 150 ) {
         ## this condition should only be true once
         ## 498000 will be struck on set 0, otherwise we would need to make it more complex
         inc <- 125
+        totalInc <- inc * qtyCores
         i <- i - 25 * set
         j <- j - 25 * (set + 1)
     }
---
+++ getData.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# blockHeight=501300
+
+blockHeight=501500
+qtyCores=36
+loopCores=$qtyCores-1
+
+inc=500
+totalInc=500*$qtyCores
+maxHeightAt500=$(((498000 / ($qtyCores * 500)) * $qtyCores * 500))
+maxHeightAt150=$(((498000 / ($qtyCores * 150)) * $qtyCores * 150))
+
+for n in {0..35}; do   # start 36 fetch loops
+    i=$((0+inc*n))
+    j=$((499+inc*n))
+    datasetNum=$n
+    count=0
+    while [ $i -lt $blockHeight ]; do
+	filesize=0
+	datasetNum=$((n+qtyCores*count))
+	while [ $filesize -lt 100 ]; do
+	    curl -s -o "./data/data$datasetNum.csv" "https://api.blockchair.com/bitcoin/outputs?q=is_spent(0),block_id($i..$j)&fields=recipient,value&export=csv"
+	    filesize= stat -c '%s' "./data/data$datasetNum.csv"
+	    # echo "curl -s -o './data/data$datasetNum.csv' 'https://api.blockchair.com/bitcoin/outputs?q=is_spent(0),block_id($i..$j)&fields=recipient,value&export=csv'" >> commands.txt
+	    # echo "wget --no-cache -O './data/data$datasetNum.csv' 'https://api.blockchair.com/bitcoin/outputs?q=is_spent\(0\),block_id\($i..$j\)&fields=recipient,value&export=csv'" >> commands.txt
+	    if [ $filesize -gt 100 ]; then
+		i=$((i+totalInc))
+		j=$((j+totalInc))
+
+		if [ $i -ge $maxHeightAt500 -a $i -eq 500 ]; then
+		    totalInc=150*qtyCores
+		    i=$((i-350*$n))
+		    j=$((j-350*($n+1)))
+		fi
+		
+		if [ $i -ge $maxHeightAt150 -a $i -eq 150 ]; then
+		    totalInc=125*qtyCores
+		    i=$((i-25*$n))
+		    j=$((j-25*($n+1)))
+		fi
+	    else
+		sleep 10s
+	    fi
+	done
+	count=$((count+1))
+    done &
+done
